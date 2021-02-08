681f1b38b2339e1472107a9dcf52547cacacb235
Ewa Zelenska <ewa.zelenska@gmail.com>
2021-02-08 13:38:15 +0100

    more renaming etc. Queen.cs mood changes automatically

--- src/Codecool.LifeOfAnts/Colony.cs
+++ src/Codecool.LifeOfAnts/Colony.cs
@@ -11,14 +11,14 @@ namespace Codecool.LifeOfAnts
     public class Colony
     {
         public readonly int Width;
-        private List<Ant> listOfAnts = new List<Ant>();
-        private Queen queen;
+        private List<Ant> _listOfAnts = new List<Ant>();
+        private Queen _queen;
 
         public Colony(int width, int amountOfSoldiers, int amountOfDrones, int amountOfWorkers)
         {
             Width = width;
-            queen = new Queen(new Position(Width / 2, Width / 2), this);
-            listOfAnts.Add(queen);
+            _queen = new Queen(new Position(Width / 2, Width / 2), this);
+            _listOfAnts.Add(_queen);
             GenerateRegularAnts<Soldier>(amountOfSoldiers);
             GenerateRegularAnts<Drone>(amountOfDrones);
             GenerateRegularAnts<Worker>(amountOfWorkers);
@@ -35,7 +35,7 @@ namespace Codecool.LifeOfAnts
         
         public void Update()
         {
-            foreach (var ant in listOfAnts)
+            foreach (var ant in _listOfAnts)
             {
                 ant.OnUpdate();
             }
@@ -73,7 +73,7 @@ namespace Codecool.LifeOfAnts
                 var randomPosition = new Position(Util.Random.Next(Width), Util.Random.Next(Width));
                 if (Activator.CreateInstance(typeof(T), new object[] { randomPosition, this }) is T ant)
                 {
-                    listOfAnts.Add(ant);
+                    _listOfAnts.Add(ant);
                 }
                 
             }
@@ -81,7 +81,7 @@ namespace Codecool.LifeOfAnts
 
         private Ant? AntAtPosition(Position position)
         {
-            foreach (var ant in listOfAnts)
+            foreach (var ant in _listOfAnts)
             {
                 if (ant.Position.X == position.X && ant.Position.Y == position.Y)
                 {
